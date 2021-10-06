a26861b6ec05c7d54351fce756f415b7a9c5050e
reshreshus <reshreshus@gmail.com>
2021-10-06 12:41:24 +0300

    micromark and json-server

---
+++ src/components/ModalView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal fixed bg-white dark:bg-deepblue-lighter shadow-2xl rounded z-10">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="sass" scoped>
+.modal
+  left: 50%
+  top: 50%
+  transform: translate3d(-50%, -50%, 0)</style>
--- src/modules/RemView.vue
+++ src/modules/RemView.vue
@@ -1,48 +1,55 @@
 <template>
   <div>
     <div v-for="(rem, idx) in currentRems" :key="idx" class="relative">
-      <div class="absolute -left-4 top-4 w-2 h-2 bg-black rounded-full" />
+      <div class="absolute -left-4 top-4 w-4 h-4 cursor-pointer" @click="openRem(rem)" >
+        <div class="w-2 h-2 bg-black rounded-full" />
+      </div>
       <div
         class="text-xl p-2"
         :class="{'bg-gray': rem.selected }"
-        @click="rem.selected = !rem.selected"
+        @click="selectRem(rem)"
       >
         {{ rem.text }}
+        <div v-if="rem.opened && rem.body" v-html="micromark(rem.body)" class="prose" />
       </div>
     </div>
+    <!-- <modal-view v-if="openedRem" /> -->
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
+import axios from 'axios'
+import { micromark } from 'micromark'
+// import ModalView from '@/components/ModalView'
 
 export default {
+  components: {
+    // ModalView,
+  },
   async setup() {
-    await new Promise(resolve => setTimeout(resolve, 1000))
+    const { data: rems } = await axios.get('http://localhost:3000/rems')
     return {
-      rems: ref([
-        {
-          text: 'VimNote',
-          selected: true,
-          archived: false,
-        },
-        {
-          text: 'With macros and shit',
-          selected: false,
-          archived: false,
-        },
-        {
-          text: 'This is archived',
-          selected: false,
-          archived: true,
-        }
-      ])
+      rems: ref(rems)
     }
   },
   computed: {
     currentRems() {
       return this.rems.filter(r => !r.archived)
     }
+  },
+  methods: {
+    selectRem(rem) {
+      rem.selected = !rem.selected
+      this.updateRem(rem)
+    },
+    openRem(rem) {
+      rem.opened = !rem.opened
+    },
+    updateRem(rem) {
+      axios.put(`http://localhost:3000/rems/${rem.id}`, rem)
+    },
+    micromark
   },
 }
 </script>
